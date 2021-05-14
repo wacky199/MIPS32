f8d88ad98474467fbfe1b13b6a3ee4d2f247803b
wacky <shivamsingh01111999@gmail.com>
2021-05-14 13:30:08 +0530

    added WB stage

--- pipe_MIPS32.v
+++ pipe_MIPS32.v
@@ -130,3 +130,19 @@ module pipe_MIPS32(
 			endcase
 		end
 
+
+		//***************WB Stage****************\\
+		
+
+	always @ (posedge clk1)
+	begin
+		if (TAKEN_BRANCH==0)
+			case(MEM_WB_type)
+				RR_ALU: Reg[MEM_WB_IR[15:11]]	<= #2 MEM_WB_ALUOut;	// "rd"
+                                RM_ALU: Reg[MEM_WB_IR[20:16]]   <= #2 MEM_WB_ALUOut;    // "rt"                             
+                                LOAD:	Reg[MEM_WB_IR[20:16]]   <= #2 MEM_WB_LMD;    // "rd"
+				HALT:	HALTED			<= #2 1'b1;
+			endcase
+		end
+
+endmodule
